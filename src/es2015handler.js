f3dd56058d0318278eab4298eeade08c534d49eb
Blake La Pierre <blakelapierre@gmail.com>
2015-11-19 15:32:17 -0800

    Need the es2015 module

---
+++ src/es2015handler.js
@@ -0,0 +1,16 @@
+var babel = require('babel-core');
+
+var es2015 = require('babel-preset-es2015');
+
+console.log(babel);
+window.addEventListener('load', function() {
+  var scripts = document.getElementsByTagName('script');
+
+  Array.prototype.forEach.call(scripts, function(script) {
+    if (script.type === 'application/es2015') {
+      var result = babel.transform(script.innerHTML, {presets: [es2015]});
+
+      eval(result.code);
+    }
+  });
+});
