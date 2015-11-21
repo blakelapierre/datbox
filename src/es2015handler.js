f112a9c392ae4ee201558e6d30d879508e9609ce
Blake La Pierre <blakelapierre@gmail.com>
2015-11-20 23:22:51 -0800

    Unneeded debug noise

--- src/es2015handler.js
+++ src/es2015handler.js
@@ -2,7 +2,6 @@ var babel = require('babel-core');
 
 var es2015 = require('babel-preset-es2015');
 
-console.log(babel);
 window.addEventListener('load', function() {
   var scripts = document.getElementsByTagName('script');
 
